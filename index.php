413f87cbff3220cf9b68e0a95ba7a54e668c57d1
carla-piazza1 <91873098+carla-piazza1@users.noreply.github.com>
2022-05-17 18:21:14 +0200

    projet fini

--- index.php
+++ index.php
@@ -1,19 +1,37 @@
+<?php
+if (isset($_GET['search'])) {
+    header('Location:recherche.php?siteSearch=');
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <script src="script.js" type="text/javascript"></script>
-    <title>autocompletion</title>
+    <link href="./style.css" rel="stylesheet">
+    <script src="script.js"></script>
 </head>
+       
 <body>
-<div class="search-container">
-	<input type="text" name="fruit" id="fruit" placeholder="Search fruit">
-	<div class="suggestions">
-		<ul></ul>
-	</div>
-</div>
+    
+    <img class="gif" src="https://c.tenor.com/c6Lq0OkRNXQAAAAi/pikachu-what.gif" alt="">
+       
+    
+    <main>
+        <img src="ttp.png" alt="trouve ton Pokémon">
+        <form method='get' action='recherche.php'>
+            <label for="siteSearch"></label>
+            <section id='test'>
+            <input type="search" id="siteSearch" name="siteSearch" autocomplete="off">
+            <div></div>
+
+<button class='btn'type="submit" name='search'>GO</button>
+        </form>
+   
+    </main>
+
 </body>
+
 </html>
--- script.js
+++ script.js
@@ -1,59 +1,33 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    const input = document.querySelector('#fruit');
-    const suggestions = document.querySelector('.suggestions ul');
+document.addEventListener("DOMContentLoaded", () => {
+    let search = document.querySelector("[name='siteSearch']")
+    let div = document.querySelectorAll('div')
+    let tab = []
+    let data = new FormData();
+    div[0].innerHTML = ''
+    document.getElementById('siteSearch').addEventListener("keyup", async () => {
+        if (search.value.length === 0) {
+            div[0].innerHTML = ''
+            tab = []
+        }else{
+            div[0].innerHTML = ''
+            tab=[]
 
-    const fruit = [ 'Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
+            data.append('siteSearch', encodeURIComponent(siteSearch.value))
 
-    function search(str) {
-        let results = [];
-        const val = str.toLowerCase();
+            let promises = [
+                fetch('pokemon.php?complete=1', { method: 'POST', body: data }).then((response) => response.json()),
+                fetch('pokemon.php?complete=2', { method: 'POST', body: data }).then((response2) => response2.json()),]
 
-        for (i = 0; i < fruit.length; i++) {
-            if (fruit[i].toLowerCase().indexOf(val) > -1) {
-                results.push(fruit[i]);
-            }
+            await Promise.allSettled(promises).then((results) => results.forEach((result) => tab.push(result)))
+                .then(function () { tab[0].value.forEach((element) =>
+                    div[0].innerHTML += `<a href=element.php?id=${element.id}>${element.name_french}</a>`+'<br>'
+                    );
+                    div[0].innerHTML += '<hr>'
+
+                    tab[1].value.forEach((element) => 
+                    div[0].innerHTML += `<a href=element.php?id=${element.id}>${element.name_french}</a>` + '<br>'
+                    );
+                })
         }
-
-        return results;
-    }
-
-    function searchHandler(e) {
-        const inputVal = e.currentTarget.value;
-        let results = [];
-        if (inputVal.length > 0) {
-            results = search(inputVal);
-        }
-        showSuggestions(results, inputVal);
-    }
-
-    function showSuggestions(results, inputVal) {
-        
-        suggestions.innerHTML = '';
-
-        if (results.length > 0) {
-            for (i = 0; i < results.length; i++) {
-                let item = results[i];
-                // Highlights only the first match
-                // TODO: highlight all matches
-                const match = item.match(new RegExp(inputVal, 'i'));
-                item = item.replace(match[0], `<strong>${match[0]}</strong>`);
-                suggestions.innerHTML += `<li>${item}</li>`;
-            }
-            suggestions.classList.add('has-suggestions');
-        } else {
-            results = [];
-            suggestions.innerHTML = '';
-            suggestions.classList.remove('has-suggestions');
-        }
-    }
-
-    function useSuggestion(e) {
-        input.value = e.target.innerText;
-        input.focus();
-        suggestions.innerHTML = '';
-        suggestions.classList.remove('has-suggestions');
-    }
-
-    input.addEventListener('keyup', searchHandler);
-    suggestions.addEventListener('click', useSuggestion);
+    })
 })
